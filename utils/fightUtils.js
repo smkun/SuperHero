e87883685743284f8cd1ce690f5317d1552cb704
smkun <skunian@yahoo.com>
2024-05-08 10:15:43 -0400

    no changes

--- utils/fightUtils.js
+++ utils/fightUtils.js
@@ -1,4 +1,5 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = (...args) =>
+    import("node-fetch").then(({ default: fetch }) => fetch(...args));
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -7,34 +8,37 @@ dotenv.config();
 function calculatePowerScore(powerstats) {
     let baseScore = 0;
     for (const stat in powerstats) {
-      if (typeof powerstats[stat] === 'number') {
-        baseScore += powerstats[stat];
-      }
+        if (typeof powerstats[stat] === "number") {
+            baseScore += powerstats[stat];
+        }
     }
     const baseScoreNumber = Number(baseScore);
     const randomBonus = Math.floor(Math.random() * 100) + 1;
     const totalScore = baseScoreNumber + randomBonus;
     return totalScore;
-  }
+}
 
 // Function to initiate image creation for a fight and return the proxy_url
 async function generateFightImage(hero1, hero2) {
     const prompt = `Comic book, superheroes fighting, Marvel, DC ${hero1.name} VS ${hero2.name} --ar 16:9`;
     try {
-        const response = await fetch("https://api.userapi.ai/midjourney/v2/imagine", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'api-key': process.env.MIDJOURNEY_API_KEY,
-            },
-            body: JSON.stringify({
-                prompt: prompt,
-                webhook_url: "https://example.com/imagine/webhook-url",
-                webhook_type: "result",
-                account_hash: "09078a70-e838-4339-a34d-c7ac36d4e14d",
-                is_disable_prefilter: false,
-            }),
-        });
+        const response = await fetch(
+            "https://api.userapi.ai/midjourney/v2/imagine",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "api-key": process.env.MIDJOURNEY_API_KEY,
+                },
+                body: JSON.stringify({
+                    prompt: prompt,
+                    webhook_url: "https://example.com/imagine/webhook-url",
+                    webhook_type: "result",
+                    account_hash: "09078a70-e838-4339-a34d-c7ac36d4e14d",
+                    is_disable_prefilter: false,
+                }),
+            }
+        );
 
         const data = await response.json();
         const taskHash = data.hash;
@@ -58,7 +62,7 @@ async function checkAndRetrieveImageUrl(taskHash) {
 
         // Check if statusData itself is the URL
         if (statusData && isValidHttpUrl(statusData)) {
-            imageUrl = statusData;  // Assume statusData is the URL if it's a valid URL
+            imageUrl = statusData; // Assume statusData is the URL if it's a valid URL
             console.log("Successfully retrieved image URL:", imageUrl); // Log successful retrieval
             break; // Exit loop if URL is obtained
         } else if (statusData && statusData.proxy_url) {
@@ -68,11 +72,15 @@ async function checkAndRetrieveImageUrl(taskHash) {
         }
 
         attempts++;
-        await new Promise(resolve => setTimeout(resolve, 2000)); // Wait before retrying
+        await new Promise((resolve) => setTimeout(resolve, 2000)); // Wait before retrying
     }
 
     if (!imageUrl) {
-        console.error("Failed to retrieve the image URL after", maxAttempts, "attempts.");
+        console.error(
+            "Failed to retrieve the image URL after",
+            maxAttempts,
+            "attempts."
+        );
         throw new Error("Failed to retrieve the image URL.");
     }
 
@@ -82,39 +90,44 @@ async function checkAndRetrieveImageUrl(taskHash) {
 // Helper function to check if a string is a valid URL
 function isValidHttpUrl(string) {
     let url;
-    
+
     try {
         url = new URL(string);
     } catch (_) {
-        return false;  
+        return false;
     }
 
     return url.protocol === "http:" || url.protocol === "https:";
 }
 
-
-
 async function getImageStatus(taskHash) {
     try {
-        const response = await fetch(`https://api.userapi.ai/midjourney/v2/status?hash=${taskHash}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'api-key': process.env.MIDJOURNEY_API_KEY, 
-            },
-        });
+        const response = await fetch(
+            `https://api.userapi.ai/midjourney/v2/status?hash=${taskHash}`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "api-key": process.env.MIDJOURNEY_API_KEY,
+                },
+            }
+        );
 
         const statusData = await response.json();
         // console.log("Task status:", statusData.status, "Image data:", statusData.result);
         // Check if the task is complete and the result is available
         if (statusData.status === "done") {
             if (statusData.result && statusData.result.proxy_url) {
-                return statusData.result.proxy_url;  // Return the URL
+                return statusData.result.proxy_url; // Return the URL
                 console.log(statusData.result.proxy_url);
             } else {
                 // No proxy_url found, even though the task is done, handle this case
-                console.error("Image generation complete, but no URL provided.");
-                throw new Error("Image generation complete, but no URL provided.");
+                console.error(
+                    "Image generation complete, but no URL provided."
+                );
+                throw new Error(
+                    "Image generation complete, but no URL provided."
+                );
             }
         } else if (statusData.status === "error") {
             // The task reported an error
@@ -129,20 +142,18 @@ async function getImageStatus(taskHash) {
     }
 }
 
-
 // Main function to handle the hero fight logic and image generation
 async function handleFight(hero1, hero2) {
     try {
         const hero1Score = calculatePowerScore(hero1.powerstats);
         const hero2Score = calculatePowerScore(hero2.powerstats);
         const winner = hero1Score > hero2Score ? hero1 : hero2;
-        const imageUrl = await generateFightImage(hero1, hero2);  // Directly store the returned imageUrl
+        const imageUrl = await generateFightImage(hero1, hero2); // Directly store the returned imageUrl
         return { hero1, hero2, winner, imageUrl };
     } catch (error) {
         console.error("Error during hero fight:", error);
         throw error;
     }
 }
-
 
 module.exports = { handleFight };
